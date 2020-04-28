ffe86edfe51216cb8404065c104e070de12fe8f3
SaberOwO <MrOdaNobunaga@gmail.com>
2020-04-28 16:26:16 +1000

    change state machine interface

--- src/main/java/com/unimelb/raftimpl/entity/StateMachine.java
+++ src/main/java/com/unimelb/raftimpl/entity/StateMachine.java
@@ -2,6 +2,9 @@ package com.unimelb.raftimpl.entity;
 
 import com.unimelb.raftimpl.rpc.LogEntry;
 
+import java.util.List;
+
 public interface StateMachine {
     void apply(LogEntry logEntry);
+    List<LogEntry> read();
 }
--- src/main/java/com/unimelb/raftimpl/entity/impl/StateMachineImpl.java
+++ src/main/java/com/unimelb/raftimpl/entity/impl/StateMachineImpl.java
@@ -3,9 +3,18 @@ package com.unimelb.raftimpl.entity.impl;
 import com.unimelb.raftimpl.entity.StateMachine;
 import com.unimelb.raftimpl.rpc.LogEntry;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class StateMachineImpl implements StateMachine {
     @Override
     public void apply(LogEntry logEntry) {
 
+    }
+
+    @Override
+    public List<LogEntry> read() {
+        List<LogEntry> logEntries = new LinkedList<>();
+        return logEntries;
     }
 }
